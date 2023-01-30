b67e8c662b53a9f2dd74fd30abce104d2d6300ed
Furkan Can Baytemur <41757315+Kaaleyah@users.noreply.github.com>
2023-01-30 11:36:37 +0300

    Initial Commit

---
+++ gradientSlider Watch App/gradientSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  gradientSliderApp.swift
+//  gradientSlider Watch App
+//
+//  Created by Furkan Can Baytemur on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct gradientSlider_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
